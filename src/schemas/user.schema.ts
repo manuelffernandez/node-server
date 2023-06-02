c3bd42eaa4360c8958b97c2a59a09908d0a8e887
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-06-02 17:55:03 -0300

    modify mongoose user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,10 +2,11 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const userSchema = new Schema({
+  _id: { type: String, _id: false },
   name: { type: String, require: true, minLength: 2, maxLength: 20 },
   middleName: { type: String, require: false, minLength: 2, maxLength: 20 },
   lastName: { type: String, require: true, minLength: 4, maxLength: 20 },
-  email: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
   password: { type: String, require: true },
   createdAt: { type: Date, require: true, default: Date.now },
   updatedAt: { type: [Date], require: false }

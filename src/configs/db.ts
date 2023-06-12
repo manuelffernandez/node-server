af2b354cb64e75b6d1fe3762018f3892cc3998df
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-06-12 04:07:26 -0300

    refactor db config file & add console.log to app entry point

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -2,15 +2,12 @@ import mongoose from 'mongoose'
 
 const connectDB = async (url: string) => {
   console.log('connecting to database...')
-  await mongoose
-    .connect(url)
-    .then(() => {
-      console.log('Database connected')
-    })
-    .catch(e => {
-      console.log('something went wrong')
-      console.log(e)
-    })
+  try {
+    await mongoose.connect(url)
+    console.log('Database connected')
+  } catch (error) {
+    console.log('something went wrong trying to connect to the database', error)
+  }
 }
 
 export default connectDB
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import httpServer from '@/configs/http'
 import connectDB from './configs/db'
 
 const bootstrap = () => {
+  console.log('starting server...')
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   httpServer.listen(process.env.PORT, async () => {
     await connectDB(process.env.MONGO_URL!)

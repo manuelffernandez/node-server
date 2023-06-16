6f54e911f4e0eb06b7fdc56f11722032931ed6cd
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-06-16 20:04:55 -0300

    remove non null assertion in env vars

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ import connectDB from './configs/db'
 
 const bootstrap = () => {
   console.log('starting server...')
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   httpServer.listen(process.env.PORT, async () => {
-    await connectDB(process.env.MONGO_URL!)
+    await connectDB(process.env.MONGO_URL)
 
-    console.log('SERVER RUNNING ON PORT ' + process.env.PORT!.toString())
+    console.log('SERVER RUNNING ON PORT ' + process.env.PORT.toString())
   })
 }
 

9467c8ad78b3817adf665eefebdde686122f13b8
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-06-19 16:42:16 -0300

    validate env func refactorized

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -27,12 +27,24 @@ const Env = {
 const EnvSchema = Type.Object(Env)
 type EnvType = Static<typeof EnvSchema>
 
-function validateEnvVars(config: any): EnvType | Error {
+function getEnvVars() {
+  const vars = {}
+
+  for (const key in Env) {
+    const value = process.env[key]
+    Object.defineProperty(vars, key, { value, enumerable: true })
+  }
+
+  return vars as Record<keyof typeof Env, string>
+}
+
+export const ENV_VARS = getEnvVars() as EnvType
+
+export function validateEnvVars() {
   const ajv = new Ajv({ allErrors: true })
   addErrors(ajv)
   const validate = ajv.compile(EnvSchema)
-  const valid = validate(config)
-
+  const valid = validate(ENV_VARS)
   if (!valid) {
     const errors = JSON.stringify(
       validate.errors!.map(error => error.message),
@@ -41,33 +53,4 @@ function validateEnvVars(config: any): EnvType | Error {
     )
     throw new Error(errors)
   }
-
-  return config as EnvType
 }
-
-function getEnvVars(): EnvType {
-  const vars = {}
-
-  for (const key in Env) {
-    const value = process.env[key]
-    Object.defineProperty(vars, key, { value, enumerable: true })
-  }
-
-  return vars as EnvType
-}
-
-function env() {
-  const ENV_VARS = getEnvVars()
-  let isEnvValid: boolean
-
-  try {
-    validateEnvVars(ENV_VARS)
-    isEnvValid = true
-  } catch (error) {
-    isEnvValid = false
-    console.log(error)
-  }
-  return { ENV_VARS, isEnvValid }
-}
-
-export const { ENV_VARS, isEnvValid } = env()

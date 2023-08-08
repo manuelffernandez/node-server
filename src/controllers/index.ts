6166e532d18fac919c0ca1c8ff86ae4203495920
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-08-08 19:56:40 -0300

    controllers barrel file

---
+++ src/controllers/index.ts
@@ -0,0 +1,3 @@
+export { default as userRegisterController } from './user-register.con'
+export { default as userLoginController } from './user-login.con'
+export { default as userProfileController } from './user-profile.con'

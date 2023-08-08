41da183c28a448d73e316a6c3ec0094544affed9
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-08-08 19:57:31 -0300

    add request type in types file

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,7 @@ import type { Static } from '@sinclair/typebox'
 import type { RegisterDTOSchema } from '@/validators/dto/user-register.dto'
 
 export type RegisterDTOType = Static<typeof RegisterDTOSchema>
+
+export interface JWTValidatedRequest extends Request {
+  id: string
+}
